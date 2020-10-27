a4d48e899323167439311a550b2f5cbeab5e947c
Rodrigo <yoyocataldo@gmail.com>
2020-10-26 22:37:31 -0300

    cuatro

--- src/Alumnos/alumnos.java
+++ src/Alumnos/alumnos.java
@@ -1,5 +1,18 @@
 package Alumnos;
+	public class alumnos {
+	    private int idAlumno;
+	    private String nombre; 
+	    private String sexo;
+	    private int edad;
+	    private Double nota;
+	    private String calificacion;
+	    public alumnos(int idAlumno, String nombre,String sexo,int edad,Double nota) {
+	        this.idAlumno = idAlumno;   this.nombre = nombre;    this.sexo=sexo;
+	        this.edad=edad; this.nota=nota;}
 
-public class alumnos {
+	    public void setNota(Double nota){this.nota=nota;}
+	    public void setCalificacion(String calificacion){this.calificacion=calificacion;}
+	    @Override
+	    public String toString() { return "ID Alumno: "+idAlumno+" Nombre: "+nombre+" Sexo: "+sexo+" Edad: "+edad+" Nota: "+nota+"  "+calificacion+"\n";  }
+	  }
 
-}
