490e21f42ac5c886a775e5d002600cb576f6a9ba
Rodrigo <yoyocataldo@gmail.com>
2020-10-27 09:52:49 -0300

    siete

--- src/Alumnos/alumnos.java
+++ src/Alumnos/alumnos.java
@@ -1,18 +1,17 @@
 package Alumnos;
-	public class alumnos {
-	    private int idAlumno;
-	    private String nombre; 
-	    private String sexo;
-	    private int edad;
-	    private Double nota;
-	    private String calificacion;
-	    public alumnos(int idAlumno, String nombre,String sexo,int edad,Double nota) {
-	        this.idAlumno = idAlumno;   this.nombre = nombre;    this.sexo=sexo;
-	        this.edad=edad; this.nota=nota;}
+public class alumnos {
+    private int idAlumnos;
+    private String nombre; 
+    private String direccion;
+    private int Rut;
+    private Double nota;
+    private String calificacion;
+    public alumnos(int idAlumnos, String nombre,String direccion,int Rut,int rut, Double nota) {
+        this.idAlumnos = idAlumnos;   this.nombre = nombre;    this.direccion=direccion;
+        this.Rut=rut; this.nota=nota;}
 
-	    public void setNota(Double nota){this.nota=nota;}
-	    public void setCalificacion(String calificacion){this.calificacion=calificacion;}
-	    @Override
-	    public String toString() { return "ID Alumno: "+idAlumno+" Nombre: "+nombre+" Sexo: "+sexo+" Edad: "+edad+" Nota: "+nota+"  "+calificacion+"\n";  }
-	  }
-
+    public void setNota(Double nota){this.nota=nota;}
+    public void setCalificacion(String calificacion){this.calificacion=calificacion;}
+    @Override
+    public String toString() { return "ID Alumno: "+idAlumnos+" Nombre: "+nombre+" Direccion: "+direccion+" Rut: "+Rut+" Nota: "+nota+"  "+calificacion+"\n";  }
+  }
